a0363a0bf42ab27f0da4469a8485bc1bbafae431
gokalpgokce <gokalpgokce7@gmail.com>
2021-09-07 14:02:08 +0300

    AddDeleteFunction

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -6,6 +6,7 @@ public class GridManager : MonoBehaviour
 {
 
     public int[,] Grid;
+    public GameObject[,] GridVisual;
     public GameObject squarePrefab;
     private GameObject Block;
     public int UserInputRun;
@@ -13,8 +14,8 @@ public class GridManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        Grid = new int[Columns, Rows];
+        Grid = new int[Rows, Columns];
+        GridVisual = new GameObject[Rows, Columns];
 
         CreateBlock(5,20);
         
@@ -25,38 +26,34 @@ public class GridManager : MonoBehaviour
     private void CreateBlock(int x, int y)
     {
         Block = Instantiate(squarePrefab, new Vector3(x,y,0), Quaternion.identity);
-        //GameObject g = new GameObject("X: " + x + "Y:" + y);
-        //g.transform.position = new Vector3(x + 0.5f, y + 0.5f);
     }
 
     public bool CheckBlockLeft(Vector3 blockPosition)
     {
-        Debug.Log("CheckBlockLeft Func");
-        int LeftMove = (int)blockPosition.x -1;
-        if(Grid[LeftMove,(int)blockPosition.y] == 1)
+        int LeftMove = (int)blockPosition.y-1;
+        if (Grid[(int) blockPosition.x, LeftMove] == 1)
+        {
             return false;
+        }
         return true;
     }
 
     public bool CheckBlockRight(Vector3 blockPosition)
     {
-        Debug.Log("CheckBlockRight Func");
-        int RightMove = (int)blockPosition.x +1;
-        if(Grid[RightMove,(int)blockPosition.y] == 1)
+        int RightMove = (int)blockPosition.y+1;
+        if (Grid[(int) blockPosition.x, RightMove] == 1)
+        {
             return false;
+        }
         return true;
     }
     public bool CheckBlockVertical(Vector3 blockPosition)
     {
-        int DownMove = (int)blockPosition.y-1;
-        if(Grid[(int)blockPosition.x,DownMove] == 1)
+        if(Grid[(int)blockPosition.x-1, (int)blockPosition.y] == 1)
         {
             return false;
         }
-        else
-        {
-            return true; 
-        }    
+        return true;
     }
 
     public void UpdateGrid(Vector3 blockPosition)
@@ -64,7 +61,9 @@ public class GridManager : MonoBehaviour
         int gridx = (int)blockPosition.x;
         int gridy = (int)blockPosition.y;
         Grid[gridx,gridy] = 1;
-        Debug.Log(Grid[gridx,gridy]);
+        Block.gameObject.name = "Blok: (" +gridx+"," +gridy+")";
+        GridVisual[gridx, gridy] = Block;
+        IsRowsFull();
         CreateBlock(5,20);
     }
 
@@ -74,16 +73,13 @@ public class GridManager : MonoBehaviour
         {
             if (UserInputRun == 0)
             {
-                Debug.Log("while dongu icerisi");
                 if (IsReachBottom(FindBlockPosition()))
                 {
-                    Debug.Log("isreachbottom true");
                     UpdateGrid(FindBlockPosition());
                     yield return null;
                 }
                 else if(CheckBlockVertical(FindBlockPosition()))
                 {
-                    Debug.Log("checkblockvert true");
                     Block.transform.position += new Vector3(0,-1,0);
                 }
                 else
@@ -105,7 +101,6 @@ public class GridManager : MonoBehaviour
             {
                 if(CheckBordersLeft(FindBlockPosition()) && CheckBlockLeft(FindBlockPosition()))
                 {
-                    //UserInputRun=1;
                     Block.transform.position += new Vector3(-1,0,0);
                 }
             }
@@ -114,7 +109,6 @@ public class GridManager : MonoBehaviour
             {
                 if(CheckBordersRight(FindBlockPosition()) && CheckBlockRight(FindBlockPosition()))
                 {
-                    //UserInputRun=1;
                     Block.transform.position += new Vector3(1,0,0);
                 }
             }
@@ -133,59 +127,67 @@ public class GridManager : MonoBehaviour
 
     public Vector3 FindBlockPosition()
     {
-        return Block.transform.position;
+        int col = Mathf.RoundToInt(Block.transform.position.x);
+        int row = Mathf.RoundToInt(Block.transform.position.y);
+        return new Vector3(row, col);
     }
     
     public bool CheckBordersLeft(Vector3 blockPosition)
     {
-        if(blockPosition.x <= 0)
+        if(blockPosition.y <= 0)
             return false;
         return true;
     }
 
     public bool CheckBordersRight(Vector3 blockPosition)
     {
-        if(blockPosition.x >=9)
+        if(blockPosition.y >=9)
             return false;
         return true;
     }
 
     public bool IsReachBottom(Vector3 blockPosition)
     {
-        if(blockPosition.y <= 0)
+        if(blockPosition.x <= 0)
         {
             return true;
         }
         return false;
     }
 
-    public bool IsFullRows()
+    public void IsRowsFull()
     {
         int ColumsCount = 0;
-        for(int x = 0 ; x < Rows ; x++)
+        for(int row = 0 ; row < Rows ; row++)
         {
-            for(int y = 0 ; y < Columns ; y++)
+            for(int col = 0 ; col < Columns ; col++)
             {
-                if(Grid[x,y] == 1)
+                if(Grid[row,col] == 1)
                 {
                     ColumsCount++;
                     if(ColumsCount == 10)
                     {
-                        return true;
+                        Debug.Log("Satir dolu delete row calisacak");
+                        DeleteRows(row);
                     }
                 }
             }
+            //Debug.Log("ColumsCount: " + ColumsCount);
             ColumsCount = 0;
         }
-        return false;
     }
 
-    public void DeleteRows()
+    public void DeleteRows(int row)
     {
-        for(int i = 0 ; i < Columns ; i++)
+        Debug.Log("delete row func silinecek satir: " + row);
+        for(int col = 0 ; col < Columns ; col++)
         {
-            
+            Debug.Log("row" + row);
+            Debug.Log("col" + col);
+            GameObject.Destroy(GridVisual[row,col]);
+            GridVisual[row, col] = null;
+            Grid[row, col] = 0;
+            Debug.Log("Grid: " + Grid[row,col]);
         }
     }
-
 }
